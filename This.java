d6114fa046c87830863eaca5e3b2521b6704912c
malli24 <malleswari82006@gmail.com>
2025-01-31 09:50:19 +0530

    Add files via upload

---
+++ This.java
@@ -0,0 +1,19 @@
+class This
+{
+    public int a;
+    public void set(int a);
+    {
+        this.a=a;
+    }
+    public void display()
+    {
+    System.out.println("a=+a");
+    }
+    public static void main(String[]args)
+    {
+        sample k=new sample();
+        k.set(10);
+        k.display();
+    }
+}
+
