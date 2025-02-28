59eaa469bef7fc67bcc620a12eddf70bc98ac5a1
malli24 <malleswari82006@gmail.com>
2025-02-28 10:22:51 +0530

    Add files via upload

---
+++ Staticmethods.java
@@ -0,0 +1,11 @@
+class Staticmethods
+{
+    private static void s()
+    {
+        System.out.println("hello students");
+    }
+    public static void main(String args[])
+    {
+        s();
+    } 
+}
