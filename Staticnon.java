e463f2ea692f6ae831257399fd74b885c1fc9198
malli24 <malleswari82006@gmail.com>
2025-02-28 10:23:11 +0530

    Add files via upload

---
+++ Staticnon.java
@@ -0,0 +1,13 @@
+class Staticnon
+{
+    public static String s()
+    {
+        return "hello students";
+    }
+    public static void main(String args[])
+    {
+        String malli=s();
+        System.out.println("malli");
+
+    } 
+}
